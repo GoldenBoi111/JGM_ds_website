b69ea49113bb79dbbfb54059c54d074d7fa583bd
Aruntej Thummepally <goldenboi2416@gmail.com>
2025-12-06 13:16:11 -0500

    Up-to date structure, vercel changes

--- DSWebsite/src/pages/BlogIndex.tsx
+++ DSWebsite/src/pages/BlogIndex.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { blogPosts } from "../components/posts";
 import { motion } from "framer-motion";
